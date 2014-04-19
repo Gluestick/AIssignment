e64c40ac0c237dcb14e54f6d76a3bf8ead4f438a
Joep Kemperman <kazooiej@gmail.com>
2014-04-19 20:23:49 +0200

    Changed public/private order in Vector2.

--- Game/Vector2.cs
+++ Game/Vector2.cs
@@ -5,11 +5,11 @@
 	/// </summary>
 	internal struct Vector2
 	{
-		public double X { get { return _x; } set { _x = value; } }
-		public double Y { get { return _y; } set { _y = value; } }
-
 		private double _x;
 		private double _y;
+
+		public double X { get { return _x; } set { _x = value; } }
+		public double Y { get { return _y; } set { _y = value; } }
 
 		/// <summary>
 		/// Creats a 2D vector with X and Y initialized to the specified values.
