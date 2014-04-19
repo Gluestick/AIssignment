e846aa7d128d794fd798a10daa5c9968c2aebb45
Joep Kemperman <kazooiej@gmail.com>
2014-04-19 22:36:25 +0200

    Added constructor foor Seek steering behavior.

--- Game/SteeringBehavior/Seek.cs
+++ Game/SteeringBehavior/Seek.cs
@@ -1,7 +1,19 @@
 ﻿namespace ISGPAI.Game.SteeringBehavior
 {
+	/// <summary>
+	/// Steering behavior that seeks a certain entity.
+	/// </summary>
 	internal class Seek : ISteeringBehavior
 	{
+		private Entity _target;
+
+		/// <summary>
+		/// Construct a steering behavior that seeks the specified target.
+		/// </summary>
+		public Seek(Entity target)
+		{
+		}
+
 		public Vector2 Steer(MovingEntity agent)
 		{
 			throw new System.NotImplementedException();
