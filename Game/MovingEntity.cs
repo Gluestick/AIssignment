d59c74ce1def2f6f0aad58d0d389ce06c922818b
Joep Kemperman <kazooiej@gmail.com>
2014-04-19 20:23:24 +0200

    Private fields in MovingEntity now exposed with public properties.

--- Game/MovingEntity.cs
+++ Game/MovingEntity.cs
@@ -9,5 +9,47 @@
 		private double _maxSpeed;
 		private double _maxForce;
 		private double _maxTurnRate;
+
+		public Vector2 Velocity
+		{
+			get { return _velocity; }
+			set { _velocity = value; }
+		}
+
+		public Vector2 Heading
+		{
+			get { return _heading; }
+			set { _heading = value; }
+		}
+
+		public Vector2 Side
+		{
+			get { return _side; }
+			set { _side = value; }
+		}
+
+		public double Mass
+		{
+			get { return _mass; }
+			set { _mass = value; }
+		}
+
+		public double MaxSpeed
+		{
+			get { return _maxSpeed; }
+			set { _maxSpeed = value; }
+		}
+
+		public double MaxForce
+		{
+			get { return _maxForce; }
+			set { _maxForce = value; }
+		}
+
+		public double MaxTurnRate
+		{
+			get { return _maxTurnRate; }
+			set { _maxTurnRate = value; }
+		}
 	}
 }
