500e4809fae8231da92b0c461ac8af19777f51aa
Joep Kemperman <kazooiej@gmail.com>
2014-04-19 22:32:51 +0200

    Added Seek steeringbehavior class.

---
+++ Game/SteeringBehavior/Seek.cs
@@ -0,0 +1,10 @@
+﻿namespace ISGPAI.Game.SteeringBehavior
+{
+	internal class Seek : ISteeringBehavior
+	{
+		public Vector2 Steer(MovingEntity agent)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
