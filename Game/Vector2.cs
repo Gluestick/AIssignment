67d4280c85d2745923e165ecefa17140ede22ee3
Joep Kemperman <kazooiej@gmail.com>
2014-04-19 20:26:06 +0200

    Added Length property to Vector2.

--- Game/Vector2.cs
+++ Game/Vector2.cs
@@ -1,4 +1,6 @@
-﻿namespace ISGPAI.Game
+﻿using System;
+
+namespace ISGPAI.Game
 {
 	/// <summary>
 	/// 2 Dimensional vector.
@@ -10,6 +12,18 @@
 
 		public double X { get { return _x; } set { _x = value; } }
 		public double Y { get { return _y; } set { _y = value; } }
+
+		/// <summary>
+		/// Gets the length of the vector.
+		/// </summary>
+		public double Length
+		{
+			get
+			{
+				// Pythagorean theorem.
+				return Math.Sqrt(X * X + Y * Y);
+			}
+		}
 
 		/// <summary>
 		/// Creats a 2D vector with X and Y initialized to the specified values.
