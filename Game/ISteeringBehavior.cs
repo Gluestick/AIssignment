642aab81f305a85d7de124539290ff1db3902181
Joep Kemperman <kazooiej@gmail.com>
2014-04-19 22:30:31 +0200

    Added MovingEntity as parameter to ISteeringBehavior.Steer()

--- Game/ISteeringBehavior.cs
+++ Game/ISteeringBehavior.cs
@@ -1,6 +1,13 @@
 ﻿namespace ISGPAI.Game
 {
+	/// <summary>
+	/// Strategy for steering behavior.
+	/// </summary>
 	internal interface ISteeringBehavior
 	{
+		/// <summary>
+		/// Calculates a force for the agent.
+		/// </summary>
+		Vector2 Steer(MovingEntity agent);
 	}
 }
