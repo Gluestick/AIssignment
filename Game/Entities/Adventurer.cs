3c4f91f57969a578d23ec7295932cf3b746fda12
Joep Kemperman <kazooiej@gmail.com>
2014-04-20 16:16:12 +0200

    Implemented Aventurer.Paint method

--- Game/Entities/Adventurer.cs
+++ Game/Entities/Adventurer.cs
@@ -11,7 +11,12 @@ namespace ISGPAI.Game.Entities
 
 		public override void Paint(Graphics g)
 		{
-			throw new System.NotImplementedException();
+			const int Size = 20;
+			g.FillEllipse(Brushes.Black,
+				(int)Position.X - Size / 2,
+				(int)Position.Y - Size / 2,
+				Size, Size
+			);
 		}
 	}
 }
