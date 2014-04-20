cd8cb07d6d4e2ba887a8dea4ac3ad05e5db0b13b
Joep Kemperman <kazooiej@gmail.com>
2014-04-20 16:18:22 +0200

    Added IPaintable interface.

---
+++ Game/IPaintable.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace ISGPAI.Game
+{
+	/// <summary>
+	/// Implementations will be able to paint on a GDI+ canvas.
+	/// </summary>
+	internal interface IPaintable
+	{
+		void Paint(Graphics g);
+	}
+}
