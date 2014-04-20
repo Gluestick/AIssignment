5cc6b70ad6e7a2ace17365c6b8ce0efa9286e34a
Joep Kemperman <kazooiej@gmail.com>
2014-04-20 14:51:40 +0200

    Added a GamePanel to draw on.

---
+++ Game/GamePanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace ISGPAI.Game
+{
+	/// <summary>
+	/// Panel used to draw the game world in.
+	/// </summary>
+	internal class GamePanel : Panel
+	{
+		/// <summary>
+		/// Game world that will be drawn on this panel.
+		/// </summary>
+		public World World { get; set; }
+
+		public GamePanel()
+		{
+			DoubleBuffered = true;
+			BackColor = Color.White;
+		}
+
+		/// <summary>
+		/// Paint the game on this panel.
+		/// </summary>
+		protected override void OnPaint(PaintEventArgs e)
+		{
+			base.OnPaint(e);
+		}
+	}
+}
