7806090fd808e1cb01cbee99b0fbb59217321e2f
Joep Kemperman <kazooiej@gmail.com>
2014-04-20 16:32:58 +0200

    Added a testworldfactory for testing purposes.

---
+++ Game/TestWorldFactory.cs
@@ -0,0 +1,17 @@
+﻿using ISGPAI.Game.Entities;
+
+namespace ISGPAI.Game
+{
+	/// <summary>
+	/// Create a test world with some entities.
+	/// </summary>
+	internal static class TestWorldFactory
+	{
+		public static World CreateWorld()
+		{
+			World world = new World();
+			world.AddEntity(new Adventurer());
+			return world;
+		}
+	}
+}
