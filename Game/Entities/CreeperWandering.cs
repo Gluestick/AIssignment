210b73e60902faf291a97417de1e7f1dc950bc4a
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 17:28:41 +0200

    Added Creeper Wandering state

---
+++ Game/Entities/CreeperWandering.cs
@@ -0,0 +1,23 @@
+﻿namespace ISGPAI.Game.Entities
+{
+	/// <summary>
+	/// Creeper state when there is no adventurer nearby.
+	/// </summary>
+	internal class CreeperWandering : State<Creeper>
+	{
+		public override void Enter(Creeper entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Update(Creeper entity, double elapsed)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Exit(Creeper entity)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
