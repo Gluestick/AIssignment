d706a117cfef9c46fb2a8a1f0479e49bb179875f
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 17:55:22 +0200

    Added StateMachine to Creeper.

--- Game/Entities/Creeper.cs
+++ Game/Entities/Creeper.cs
@@ -5,9 +5,11 @@ namespace ISGPAI.Game.Entities
 	internal class Creeper : MovingEntity
 	{
 		private World _world;
+		private StateMachine<Creeper> _stateMachine;
 
 		public Creeper(World world)
 		{
+			this._stateMachine = new StateMachine<Creeper>(this);
 			this._world = world;
 		}
 
