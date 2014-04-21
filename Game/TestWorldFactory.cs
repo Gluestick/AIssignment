18b9b4d99ae97c12f618aa8be75d59a4d1b3ed3a
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 14:54:19 +0200

    Changed Adventurer's starting position in TestWorldFactory to 0,0.

--- Game/TestWorldFactory.cs
+++ Game/TestWorldFactory.cs
@@ -10,12 +10,7 @@ namespace ISGPAI.Game
 		public static World CreateWorld()
 		{
 			World world = new World();
-			world.AddEntity(
-				new Adventurer()
-				{
-					Position = new Vector2(100, 100)
-				}
-			);
+			world.AddEntity(new Adventurer());
 			return world;
 		}
 	}
