11ca1d7865769507cb13f1ece6a4f789e3af8ab7
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 17:30:20 +0200

    Creeper now has a contsructor which takes a World object.

--- Game/Entities/Creeper.cs
+++ Game/Entities/Creeper.cs
@@ -4,6 +4,13 @@ namespace ISGPAI.Game.Entities
 {
 	internal class Creeper : MovingEntity
 	{
+		private World _world;
+
+		public Creeper(World world)
+		{
+			this._world = world;
+		}
+
 		public override void Update(double elapsed)
 		{
 			throw new System.NotImplementedException();
