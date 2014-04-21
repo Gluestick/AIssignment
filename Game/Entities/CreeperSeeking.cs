f59f42f41033cbe09564a18539843594882b0d67
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 16:27:30 +0200

    Added CreeperSeeking state.

---
+++ Game/Entities/CreeperSeeking.cs
@@ -0,0 +1,20 @@
+﻿namespace ISGPAI.Game.Entities
+{
+	internal class CreeperSeeking : State<Creeper>
+	{
+		public override void Enter(Creeper entity)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Update(Creeper entity, double elapsed)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Exit(Creeper entity)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
