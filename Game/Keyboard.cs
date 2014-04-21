743963173b398cb282722cb54b4823adc13844b6
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 12:50:47 +0200

    Added Keyboard class.

---
+++ Game/Keyboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ISGPAI.Game
+{
+	/// <summary>
+	/// Static class with methods to inspect the keyboard's state.
+	/// </summary>
+	internal static class Keyboard
+	{
+		[DllImport("user32.dll")]
+		private static extern short GetAsyncKeyState(Keys key);
+
+		/// <summary>
+		/// Checks if the specified key is currently being pressed.
+		/// </summary>
+		public static bool IsKeyDown(Keys key)
+		{
+			return GetAsyncKeyState(key) != 0;
+		}
+	}
+}
