a24796cb73f02d6510ae5e82eccf767ebbfd2900
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 16:25:47 +0200

    Added abstract methods to State.

--- Game/Entities/State.cs
+++ Game/Entities/State.cs
@@ -1,7 +1,10 @@
 ﻿namespace ISGPAI.Game.Entities
 {
-	internal class State<T>
+	internal abstract class State<T>
 		where T : Entity
 	{
+		public abstract void Enter(T entity);
+		public abstract void Update(T entity, double elapsed);
+		public abstract void Exit(T entity);
 	}
 }
