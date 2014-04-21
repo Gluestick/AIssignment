ed5e91e0cec199f703523a4dd465ee7c07c38a56
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 18:44:00 +0200

    Implemented CreeperSeeking.

--- Game/Entities/CreeperSeeking.cs
+++ Game/Entities/CreeperSeeking.cs
@@ -1,4 +1,7 @@
-﻿namespace ISGPAI.Game.Entities
+﻿using System.Diagnostics;
+using ISGPAI.Game.SteeringBehaviors;
+
+namespace ISGPAI.Game.Entities
 {
 	/// <summary>
 	/// Creeper state when the creeper is nearby the adventurer.
@@ -6,26 +9,34 @@
 	internal class CreeperSeeking : State<Creeper>
 	{
 		private Entity _target;
+		private ISteeringBehavior _steeringBehavior;
 
 		public CreeperSeeking(Creeper agent, Entity target)
 			: base(agent)
 		{
 			this._target = target;
+			_steeringBehavior = new SeekSteering(target);
 		}
 
 		public override void Enter()
 		{
-			throw new System.NotImplementedException();
+			Debug.WriteLine("Creeper {0} starts seeking {1}.",
+				_agent.Id, _target.Id);
 		}
 
 		public override void Update(double elapsed)
 		{
-			throw new System.NotImplementedException();
+			Vector2 steeringForce = _steeringBehavior.Steer(_agent);
+			Vector2 acceleration = steeringForce / _agent.Mass;
+			_agent.Velocity += acceleration * elapsed;
+			_agent.Velocity = _agent.Velocity.Truncate(_agent.MaxSpeed);
+			_agent.Position += _agent.Velocity * elapsed;
 		}
 
 		public override void Exit()
 		{
-			throw new System.NotImplementedException();
+			Debug.WriteLine("Target {0} is out of sight. Creeper {1} goes back to wandering.",
+				_target.Id, _agent.Id);
 		}
 	}
 }
