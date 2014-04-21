144b927a2abc14d7b8c2e5a16e2516af8ea41545
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 19:59:41 +0200

    Added WanderSteering class.

---
+++ Game/SteeringBehaviors/WanderSteering.cs
@@ -0,0 +1,15 @@
+﻿using ISGPAI.Game.Entities;
+
+namespace ISGPAI.Game.SteeringBehaviors
+{
+	/// <summary>
+	/// Returns a force that makes an entity wander randomly around the world.
+	/// </summary>
+	internal class WanderSteering : ISteeringBehavior
+	{
+		public Vector2 Steer(MovingEntity agent)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
