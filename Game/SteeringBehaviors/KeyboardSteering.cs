20cda1e88d0ad8c95caeb43d98af84a3a799d784
Joep Kemperman <kazooiej@gmail.com>
2014-04-21 12:18:38 +0200

    Added KeyboardSteering class.

---
+++ Game/SteeringBehaviors/KeyboardSteering.cs
@@ -0,0 +1,15 @@
+﻿using ISGPAI.Game.Entities;
+
+namespace ISGPAI.Game.SteeringBehaviors
+{
+	/// <summary>
+	/// Returns a force in the direction of the current arrow key state.
+	/// </summary>
+	internal class KeyboardSteering : ISteeringBehavior
+	{
+		public Vector2 Steer(MovingEntity agent)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
