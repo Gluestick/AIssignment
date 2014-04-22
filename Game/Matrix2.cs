7e52314e4f8fb03a6a15f3370bfd22579fcfc9c6
Joep Kemperman <kazooiej@gmail.com>
2014-04-22 21:03:40 +0200

    Implemented Matrix2.MatrixMultiply.

--- Game/Matrix2.cs
+++ Game/Matrix2.cs
@@ -88,6 +88,57 @@
 
 		private void MatrixMultiply(Matrix mat)
 		{
+			Matrix tmpMatrix = new Matrix();
+
+			// First row
+			tmpMatrix._11 =
+				(_matrix._11 * mat._11) +
+				(_matrix._12 * mat._21) +
+				(_matrix._13 * mat._31);
+
+			tmpMatrix._12 =
+				(_matrix._11 * mat._12) +
+				(_matrix._12 * mat._22) +
+				(_matrix._13 * mat._32);
+
+			tmpMatrix._13 =
+				(_matrix._11 * mat._13) +
+				(_matrix._12 * mat._23) +
+				(_matrix._13 * mat._33);
+
+			// Second row
+			tmpMatrix._21 =
+				(_matrix._21 * mat._11) +
+				(_matrix._22 * mat._21) +
+				(_matrix._23 * mat._31);
+
+			tmpMatrix._22 =
+				(_matrix._21 * mat._12) +
+				(_matrix._22 * mat._22) +
+				(_matrix._23 * mat._32);
+
+			tmpMatrix._23 =
+				(_matrix._21 * mat._13) +
+				(_matrix._22 * mat._23) +
+				(_matrix._23 * mat._33);
+
+			// Third row
+			tmpMatrix._31 =
+				(_matrix._31 * mat._11) +
+				(_matrix._32 * mat._21) +
+				(_matrix._33 * mat._31);
+
+			tmpMatrix._32 =
+				(_matrix._31 * mat._12) +
+				(_matrix._32 * mat._22) +
+				(_matrix._33 * mat._32);
+
+			tmpMatrix._33 =
+				(_matrix._31 * mat._13) +
+				(_matrix._32 * mat._23) +
+				(_matrix._33 * mat._33);
+
+			_matrix = tmpMatrix;
 		}
 	}
 }
