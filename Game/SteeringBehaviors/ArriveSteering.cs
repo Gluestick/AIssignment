439d9029adc8b8beaf5cf6c8028467754a7acf27
Joep Kemperman <kazooiej@gmail.com>
2014-04-23 21:52:55 +0200

    Added ArriveSteering (needed for leader following).

---
+++ Game/SteeringBehaviors/ArriveSteering.cs
@@ -0,0 +1,13 @@
+﻿using ISGPAI.Game.Entities;
+using ISGPAI.Game.Maths;
+
+namespace ISGPAI.Game.SteeringBehaviors
+{
+	internal class ArriveSteering : ISteeringBehavior
+	{
+		public Vector2 Steer(MovingEntity agent, double elapsed)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
