4e9b9d5296881cbe01fe1e165d1dc8ceac34c931
Joep Kemperman <kazooiej@gmail.com>
2014-04-23 22:03:43 +0200

    implemented follower.paint.

--- Game/Entities/Follower.cs
+++ Game/Entities/Follower.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +16,12 @@ namespace ISGPAI.Game.Entities
 
 		public override void Paint(System.Drawing.Graphics g)
 		{
-			throw new NotImplementedException();
+			const int Size = 10;
+			g.FillEllipse(Brushes.Red,
+				(int)Position.X - Size / 2,
+				(int)Position.Y - Size / 2,
+				Size, Size
+			);
 		}
 	}
 }
