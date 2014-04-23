9884aa3ce08cb65faa26e52cb0634647984156fe
Joep Kemperman <kazooiej@gmail.com>
2014-04-23 22:02:52 +0200

    Added Follower Entity.

---
+++ Game/Entities/Creeper/Follower.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISGPAI.Game.Entities
+{
+	internal class Follower : MovingEntity
+	{
+		public override void Update(double elapsed)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Paint(System.Drawing.Graphics g)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
