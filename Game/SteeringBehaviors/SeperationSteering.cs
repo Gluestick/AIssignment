1f0e8ca61c2da2dbc3af6ee655cd3a8933889e2f
Joep Kemperman <kazooiej@gmail.com>
2014-04-23 22:11:48 +0200

    Added description to SeperationSteering.

--- Game/SteeringBehaviors/SeperationSteering.cs
+++ Game/SteeringBehaviors/SeperationSteering.cs
@@ -3,6 +3,9 @@ using ISGPAI.Game.Maths;
 
 namespace ISGPAI.Game.SteeringBehaviors
 {
+	/// <summary>
+	/// Makes the agent move away from any nearby entities.
+	/// </summary>
 	internal class SeperationSteering : ISteeringBehavior
 	{
 		public Vector2 Steer(MovingEntity agent, double elapsed)
