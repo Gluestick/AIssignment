1960a55c7e84d1be5e9cbf9ffa6ffade8d105458
Joep Kemperman <kazooiej@gmail.com>
2014-04-23 21:59:44 +0200

    Implemented ArriveSteering.Steer.

--- Game/SteeringBehaviors/ArriveSteering.cs
+++ Game/SteeringBehaviors/ArriveSteering.cs
@@ -1,8 +1,12 @@
-﻿using ISGPAI.Game.Entities;
+﻿using System;
+using ISGPAI.Game.Entities;
 using ISGPAI.Game.Maths;
 
 namespace ISGPAI.Game.SteeringBehaviors
 {
+	/// <summary>
+	/// Steering behavior that steers towards a target, but slows down when close.
+	/// </summary>
 	internal class ArriveSteering : ISteeringBehavior
 	{
 		private Entity _target;
@@ -14,7 +18,20 @@ namespace ISGPAI.Game.SteeringBehaviors
 
 		public Vector2 Steer(MovingEntity agent, double elapsed)
 		{
-			throw new System.NotImplementedException();
+			const double Deceleration = 1;
+
+			Vector2 toTarget = _target.Position - agent.Position;
+			double distance = toTarget.Length;
+			if (distance > 0)
+			{
+				double speed = distance / Deceleration;
+				speed = Math.Min(speed, agent.MaxSpeed);
+				Vector2 desiredVelocity = toTarget * speed / distance;
+				return desiredVelocity - agent.Velocity;
+			}
+
+			// We're ON our target... Don't move.
+			return new Vector2(0, 0);
 		}
 	}
 }
