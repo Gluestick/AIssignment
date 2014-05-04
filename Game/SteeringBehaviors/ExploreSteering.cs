3fada80306eb5c64098200dbe2b0ea0f53fa27af
Joep Kemperman <kazooiej@gmail.com>
2014-05-04 16:12:31 +0200

    Added not implemented ExploreSteering.

---
+++ Game/SteeringBehaviors/ExploreSteering.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ISGPAI.Game.Entities;
+using ISGPAI.Game.Maths;
+
+namespace ISGPAI.Game.SteeringBehaviors
+{
+	internal class ExploreSteering : ISteeringBehavior
+	{
+		public Vector2 Steer(MovingEntity agent, double elapsed)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
