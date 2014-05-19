141219bd68a49be14d7e954578adec045034911a
Joep Kemperman <mail@joepkemperman.nl>
2014-05-19 21:10:21 +0200

    Implemented Euclidean distance heuristic.

--- Game/Collections/EuclideanDistanceHeuristic.cs
+++ Game/Collections/EuclideanDistanceHeuristic.cs
@@ -1,10 +1,25 @@
-﻿namespace ISGPAI.Game.Collections
+﻿using ISGPAI.Game.Maths;
+
+namespace ISGPAI.Game.Collections
 {
+	/// <summary>
+	/// AStar heuristic that calculates the euclidean distance between the
+	/// destination node of the given edge in Calculate() and the destination
+	/// specified in the constructor.
+	/// </summary>
 	internal class EuclideanDistanceHeuristic : IAStarHeuristic
 	{
+		private GraphNode _destination;
+
+		public EuclideanDistanceHeuristic(GraphNode destination)
+		{
+			this._destination = destination;
+		}
+
 		public int Calculate(GraphEdge edge)
 		{
-			throw new System.NotImplementedException();
+			Vector2 distance = edge.Destination.Position - _destination.Position;
+			return (int)distance.Length;
 		}
 	}
 }
