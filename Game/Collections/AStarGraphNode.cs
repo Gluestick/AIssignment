4c1324954fc969dfba7c1467e5a046dcf825bb78
Joep Kemperman <mail@joepkemperman.nl>
2014-05-25 19:13:16 +0200

    Added Score, BestDistance and EstimatedDistance properties to AStarGraphNode.

--- Game/Collections/AStarGraphNode.cs
+++ Game/Collections/AStarGraphNode.cs
@@ -6,6 +6,27 @@
 		{
 			public GraphNode GraphNode { get; set; }
 
+			/// <summary>
+			/// Sum of the best distance and estimated distance.
+			/// </summary>
+			public float Score
+			{
+				get
+				{
+					return BestDistance + EstimatedDistance;
+				}
+			}
+
+			/// <summary>
+			/// The best distance that has been calculated for this node so far.
+			/// </summary>
+			public float BestDistance { get; set; }
+
+			/// <summary>
+			/// Estimated distance to the destination.
+			/// </summary>
+			public float EstimatedDistance { get; set; }
+
 			public AStarGraphNode(GraphNode node)
 			{
 				this.GraphNode = node;
