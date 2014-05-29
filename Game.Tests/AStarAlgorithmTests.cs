4d4baa5a3281c41b4b0b8bc642ac2ddbd2dd05b1
Joep Kemperman <mail@joepkemperman.nl>
2014-05-29 16:21:31 +0200

    Added a test for AStar.

--- Game.Tests/AStarAlgorithmTests.cs
+++ Game.Tests/AStarAlgorithmTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using ISGPAI.Game.Collections;
 using ISGPAI.Game.Maths;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -26,6 +28,28 @@ namespace Game.Tests
 			// Should throw an argument exception, because it cannot
 			// calculate the shortest path between disconnected nodes.
 			astar.GetShortestPath(source, destination);
+		}
+
+		[TestMethod]
+		public void ConnectedGraph_GetShortestPath_ReturnsCorrectPath()
+		{
+			// Create a graph with four nodes, connected in a straight line.
+			Graph graph = new Graph();
+
+			const int NodeCount = 4;
+			GraphNode start = new GraphNode(new Vector2(0f, 0f));
+			GraphNode midFirst = new GraphNode(new Vector2(1f, 1f));
+			GraphNode midSecond = new GraphNode(new Vector2(2f, 2f));
+			GraphNode destination = new GraphNode(new Vector2(3f, 3f));
+
+			graph.AddEdge(new GraphEdge(start, midFirst));
+			graph.AddEdge(new GraphEdge(midFirst, midSecond));
+			graph.AddEdge(new GraphEdge(midSecond, destination));
+
+			AStarAlgorithm astar = new AStarAlgorithm(graph);
+			IEnumerable<GraphNode> path = astar.GetShortestPath(start, destination);
+			Assert.IsTrue(path.Count() == NodeCount,
+				"Path has an invalid amount of nodes");
 		}
 	}
 }
