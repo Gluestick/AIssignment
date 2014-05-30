23b852276ea12ea96a528779cd09e73827ffe650
Joep Kemperman <mail@joepkemperman.nl>
2014-05-30 21:12:53 +0200

    Implemented SeekAtSteering.Steer method.

--- Game/SteeringBehaviors/SeekAtSteering.cs
+++ Game/SteeringBehaviors/SeekAtSteering.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using ISGPAI.Game.Entities;
+﻿using ISGPAI.Game.Entities;
 using ISGPAI.Game.Maths;
 
 namespace ISGPAI.Game.SteeringBehaviors
 {
 	internal class SeekAtSteering : ISteeringBehavior
 	{
+		public Vector2 Location { get; set; }
+
+		public SeekAtSteering(Vector2 location)
+		{
+			this.Location = location;
+		}
+
 		public Vector2 Steer(MovingEntity agent, double elapsed)
 		{
-			throw new NotImplementedException();
+			Vector2 desiredVelocity = Vector2.Normalize(
+				this.Location - agent.Position
+			) * agent.MaxSpeed;
+			return desiredVelocity - agent.Velocity;
 		}
 	}
 }
