b48bcadc73b22c49bbb95e702aa5e83ba74f8741
Joep Kemperman <mail@joepkemperman.nl>
2014-05-30 16:09:43 +0200

    Added class for depth first search using IEnumerable and IEnumerator interface.

---
+++ Game/Collections/DepthFirstGraph.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace ISGPAI.Game.Collections
+{
+	/// <summary>
+	/// IEnumerable type for depth first search through a graph.
+	/// </summary>
+	public class DepthFirstGraph : IEnumerable<GraphNode>
+	{
+		private Graph _graph;
+
+		public DepthFirstGraph(Graph graph)
+		{
+			this._graph = graph;
+		}
+
+		public IEnumerator<GraphNode> GetEnumerator()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public class DepthFirstGraphEnumerator : IEnumerator<GraphNode>
+		{
+			public GraphNode Current
+			{
+				get { throw new System.NotImplementedException(); }
+			}
+
+			public void Dispose()
+			{
+				throw new System.NotImplementedException();
+			}
+
+			object System.Collections.IEnumerator.Current
+			{
+				get { throw new System.NotImplementedException(); }
+			}
+
+			public bool MoveNext()
+			{
+				throw new System.NotImplementedException();
+			}
+
+			public void Reset()
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+	}
+}
