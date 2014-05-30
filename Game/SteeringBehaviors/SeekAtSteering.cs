1e99d7e6923633320ffceb05449985b3c55a9283
Joep Kemperman <mail@joepkemperman.nl>
2014-05-30 20:01:36 +0200

    Added SeekAtSteering.

---
+++ Game/SteeringBehaviors/SeekAtSteering.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ISGPAI.Game.Entities;
+using ISGPAI.Game.Maths;
+
+namespace ISGPAI.Game.SteeringBehaviors
+{
+	internal class SeekAtSteering : ISteeringBehavior
+	{
+		public Vector2 Steer(MovingEntity agent, double elapsed)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
