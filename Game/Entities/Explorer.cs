b468b22caac886b1c870ee62e9191e2b610a0d65
Joep Kemperman <mail@joepkemperman.nl>
2014-05-30 19:25:35 +0200

    Added Explorer class.

---
+++ Game/Entities/Explorer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+
+namespace ISGPAI.Game.Entities
+{
+	/// <summary>
+	/// Moving entity that explorers the world graph. When the explorer is
+	/// done exploring, it will start over, beginning at his current position.
+	/// </summary>
+	internal class Explorer : MovingEntity
+	{
+		private World _world;
+
+		public Explorer(World world)
+		{
+			this._world = world;
+		}
+
+		public override void Update(double elapsed)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Paint(Graphics g)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
