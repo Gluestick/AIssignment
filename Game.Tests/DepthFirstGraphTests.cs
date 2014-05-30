9fb32e2c4ca78508cc5c94f6ca7bfeb5e8d43dd2
Joep Kemperman <mail@joepkemperman.nl>
2014-05-30 18:16:46 +0200

    Wrote two unit tests for DepthFirstGraph.

---
+++ Game.Tests/DepthFirstGraphTests.cs
@@ -0,0 +1,47 @@
+﻿using ISGPAI.Game.Collections;
+using ISGPAI.Game.Maths;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Game.Tests
+{
+	[TestClass]
+	public class DepthFirstGraphTests
+	{
+		[TestMethod]
+		public void ConnectedGraph_GetDepthFirstEnumerable_StartsWithStartNode()
+		{
+			Graph graph = new Graph();
+			GraphNode start = new GraphNode(new Vector2(0, 0));
+			GraphNode finish = new GraphNode(new Vector2(1, 1));
+			graph.AddEdge(new GraphEdge(start, finish));
+			var enumerator = graph.GetDepthFirstEnumerable(start).GetEnumerator();
+			Assert.IsTrue(enumerator.Current == start,
+				"Enumerator did not start with the correct node");
+		}
+
+		[TestMethod]
+		[Timeout(1000)]
+		public void ConnectedGraph_GetDepthFirstEnumerable_TraversesCorrectAmountOfNodes()
+		{
+			const int NodeCount = 5;
+			Graph graph = new Graph();
+			GraphNode start = new GraphNode(new Vector2());
+			GraphNode currentSource = start;
+			GraphNode currentDestination;
+			for (int i = 0; i < NodeCount; i++)
+			{
+				currentDestination = new GraphNode(new Vector2());
+				graph.AddEdge(new GraphEdge(currentSource, currentDestination));
+				currentSource = currentDestination;
+			}
+			var enumerator = graph.GetDepthFirstEnumerable(start).GetEnumerator();
+			int actualCount = 1;
+			while (enumerator.MoveNext())
+			{
+				actualCount++;
+			}
+			Assert.AreEqual(NodeCount, actualCount,
+				"DeothFirstEnumerator did not traverse the correct amount of nodes.");
+		}
+	}
+}
