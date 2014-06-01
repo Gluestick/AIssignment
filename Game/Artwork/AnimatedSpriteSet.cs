3fe76b121919d7b285acd3397e1bf23af64fef26
Joep Kemperman <mail@joepkemperman.nl>
2014-06-01 23:34:39 +0200

    Completed a simple version of the AnimatedSpriteSet (paints the entire spriteset).

---
+++ Game/Artwork/AnimatedSpriteSet.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using ISGPAI.Game.Maths;
+
+namespace ISGPAI.Game.Artwork
+{
+	internal class AnimatedSpriteSet
+	{
+		private Image _spriteSet;
+		private int _spriteWidth;
+		private int _spriteHeight;
+
+		public AnimatedSpriteSet(string fileName,
+			int spriteWidth, int spriteHeight)
+		{
+			this._spriteWidth = spriteWidth;
+			this._spriteHeight = spriteHeight;
+
+			_spriteSet = Image.FromFile(fileName);
+		}
+
+		public void PaintAt(Graphics g, Vector2 position)
+		{
+			g.DrawImage(_spriteSet, (int)position.X, (int)position.Y);
+		}
+	}
+}
