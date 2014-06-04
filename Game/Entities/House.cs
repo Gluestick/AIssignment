93cd9cc499ca421608caa28cb5b693b9f2d42588
Joep Kemperman <mail@joepkemperman.nl>
2014-06-04 23:11:08 +0200

    Added House class.

---
+++ Game/Entities/House.cs
@@ -0,0 +1,15 @@
+﻿namespace ISGPAI.Game.Entities
+{
+	internal class House : Entity
+	{
+		public override void Update(double elapsed)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Paint(System.Drawing.Graphics g)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
