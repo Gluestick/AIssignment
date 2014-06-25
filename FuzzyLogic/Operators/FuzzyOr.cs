6b419846bd27e16d19cd120b22ca773fa844086c
Joep Kemperman <mail@joepkemperman.nl>
2014-06-25 21:07:56 +0200

    Added Fuzzyor operator.

---
+++ FuzzyLogic/Operators/FuzzyOr.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace FuzzyLogic.Operators
+{
+	public class FuzzyOr : FuzzyOperator
+	{
+		public FuzzyOr(IEnumerable<IFuzzyTerm> terms)
+		{
+			Operate(terms);
+		}
+
+		public override double Value
+		{
+			get
+			{
+				double highest = double.MinValue;
+				foreach (IFuzzyTerm term in _terms)
+				{
+					if (term.Value > highest)
+					{
+						highest = term.Value;
+					}
+				}
+				return highest;
+			}
+		}
+	}
+}
