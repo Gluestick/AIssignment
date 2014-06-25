01f3fd3694e4f491b957ebfc05d199e6c6c988ce
Joep Kemperman <mail@joepkemperman.nl>
2014-06-25 19:54:38 +0200

    Some changes.

---
+++ FuzzyLogic/Hedges/FuzzyFairly.cs
@@ -0,0 +1,27 @@
+﻿namespace FuzzyLogic.Hedges
+{
+	public class FuzzyFairly : IFuzzyTerm
+	{
+		public double Value
+		{
+			get
+			{
+				throw new System.NotImplementedException();
+			}
+			set
+			{
+				throw new System.NotImplementedException();
+			}
+		}
+
+		public void ORWithValue(double p)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void ClearValue()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
