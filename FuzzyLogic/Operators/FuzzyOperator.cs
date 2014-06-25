523c89b7a550b4bdc410a3e4c110e8a0ab41774c
Joep Kemperman <mail@joepkemperman.nl>
2014-06-25 20:47:02 +0200

    Added base operator class.

---
+++ FuzzyLogic/Operators/FuzzyOperator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace FuzzyLogic.Operators
+{
+	public abstract class FuzzyOperator : IFuzzyTerm
+	{
+		protected ICollection<IFuzzyTerm> _terms;
+
+		public double Value
+		{
+			get { throw new System.NotImplementedException(); }
+		}
+
+		public FuzzyOperator()
+		{
+			_terms = new List<IFuzzyTerm>();
+		}
+
+		public void Operate(IEnumerable<IFuzzyTerm> terms)
+		{
+			foreach (IFuzzyTerm term in terms)
+			{
+				_terms.Add(term);
+			}
+		}
+
+		public void ORWithValue(double p)
+		{
+			foreach (IFuzzyTerm term in _terms)
+			{
+				term.ORWithValue(p);
+			}
+		}
+
+		public void ClearValue()
+		{
+			foreach (IFuzzyTerm term in _terms)
+			{
+				term.ClearValue();
+			}
+		}
+	}
+}
