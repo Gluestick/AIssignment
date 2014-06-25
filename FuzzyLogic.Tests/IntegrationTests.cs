bebd0553dee5ca45c215b1548dd4ae4daea754a5
Joep Kemperman <mail@joepkemperman.nl>
2014-06-25 21:59:17 +0200

    FuzzyTest now has an initialize method.

--- FuzzyLogic.Tests/IntegrationTests.cs
+++ FuzzyLogic.Tests/IntegrationTests.cs
@@ -5,8 +5,31 @@ namespace FuzzyLogic.Tests
 	[TestClass]
 	public class IntegrationTests
 	{
+		private FuzzyModule _module;
+
+		[TestInitialize]
+		public void Initialize()
+		{
+			_module = new FuzzyModule();
+
+			FuzzyVariable ammo = _module.CreateFLV("ammo");
+			IFuzzySet ammoLow = ammo.AddTriangularSet("low", 0, 2, 8);
+			IFuzzySet ammoDecent = ammo.AddTriangularSet("decent", 2, 8, 16);
+			IFuzzySet ammoPlenty = ammo.AddRightShoulderSet("plenty", 8, 16, 32);
+
+			FuzzyVariable desirability = _module.CreateFLV("desirability");
+			IFuzzySet undesirable = desirability.AddLeftShoulderSet("undesirable", 0, 25, 50);
+			IFuzzySet desirable = desirability.AddTriangularSet("desirable", 25, 50, 75);
+			IFuzzySet veryDesirable = desirability.AddRightShoulderSet("very_desirable", 50, 75, 100);
+
+			// Desirability for reloading.
+			_module.AddRule(ammoLow, veryDesirable);
+			_module.AddRule(ammoDecent, desirable);
+			_module.AddRule(ammoPlenty, ammoPlenty);
+		}
+
 		[TestMethod]
-		public void TestMethod()
+		public void Test()
 		{
 		}
 	}
