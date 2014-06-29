4493b02cb31089665d2ba5e4d8942f96b1d1a469
Joep Kemperman <mail@joepkemperman.nl>
2014-06-29 23:19:06 +0200

    Added creeper helper class.

---
+++ Game/Entities/CreeperHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Drawing;
+
+namespace ISGPAI.Game.Entities
+{
+	public class CreeperHelper : MovingEntity
+	{
+		private int _safety;
+		private int _helping;
+		private int _energy;
+
+		public CreeperHelper()
+		{
+			MaxSpeed = 100;
+			Mass = 1;
+		}
+
+		public override void Update(double elapsed)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Paint(Graphics g)
+		{
+			throw new NotImplementedException();
+		}
+
+		// Score check methods
+		private int ScoreAfterFleeing()
+		{
+			int score = (int)Math.Pow(Math.Max(10, _safety + 4), 2);
+			score += (int)Math.Pow(Math.Max(0, _helping - 2), 2);
+			score += (int)Math.Pow(Math.Max(0, _energy - 1), 2);
+			return score;
+		}
+
+		private int ScoreAfterAttacking()
+		{
+			int score = (int)Math.Pow(Math.Min(0, _safety - 2), 2);
+			score += (int)Math.Pow(Math.Min(10, _helping + 2), 2);
+			score += (int)Math.Pow(Math.Max(0, _energy - 2), 2);
+			return score;
+		}
+
+		private int ScoreAfterResting()
+		{
+			int score = (int)Math.Pow(_safety, 2);
+			score += (int)Math.Pow(Math.Max(0, _helping - 2), 2);
+			score += (int)Math.Pow(Math.Min(10, _energy + 2), 2);
+			return score;
+		}
+
+		// Action methods
+		private void Flee()
+		{
+			_safety = Math.Min(10, _safety - 2);
+			_safety = Math.Max(0, _safety - 2);
+			_safety = Math.Max(0, _safety - 2);
+			throw new NotImplementedException();
+		}
+
+		private void Attack()
+		{
+			_safety = Math.Min(0, _safety - 2);
+			_energy = Math.Min(0, _energy - 2);
+			_helping = Math.Min(10, _helping + 2);
+			throw new NotImplementedException();
+		}
+
+		private void Rest()
+		{
+			_helping = Math.Min(0, _helping - 2);
+			_energy = Math.Max(10, _energy + 2);
+			throw new NotImplementedException();
+		}
+	}
+}
