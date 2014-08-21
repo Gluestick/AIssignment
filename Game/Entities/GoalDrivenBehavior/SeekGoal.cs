405ce7cf294da03ef8f3eaad4d9c2145bc13d125
Joep Kemperman <mail@joepkemperman.nl>
2014-08-21 22:49:33 +0200

    Added SeekGoal with constructor (no methods implemented yet)

---
+++ Game/Entities/GoalDrivenBehavior/SeekGoal.cs
@@ -0,0 +1,35 @@
+﻿using ISGPAI.Game.Maths;
+
+namespace ISGPAI.Game.Entities.GoalDrivenBehavior
+{
+	public class SeekGoal : Goal<Helper>
+	{
+		private Vector2 _target;
+
+		public SeekGoal(Helper owner, Vector2 target)
+		{
+			this._owner = owner;
+			this._target = target;
+		}
+
+		public override void Activate()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override Status Process()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Terminate()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void AddSubGoal(Goal<Helper> goal)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
