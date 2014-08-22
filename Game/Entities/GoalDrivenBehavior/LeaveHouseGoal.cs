d0e46cfb7525aa9b5a0db7dc67956c32ea0ff3a9
Joep Kemperman <mail@joepkemperman.nl>
2014-08-22 20:40:54 +0200

    Added LeaveHouseGoal.

---
+++ Game/Entities/GoalDrivenBehavior/LeaveHouseGoal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ISGPAI.Game.Entities.GoalDrivenBehavior
+{
+	public class LeaveHouseGoal : Goal<Helper>
+	{
+		public LeaveHouseGoal(Helper owner)
+		{
+			this._owner = owner;
+		}
+
+		public override void Activate()
+		{
+			_status = Status.Active;
+		}
+
+		public override Status Process()
+		{
+			Activate();
+			_owner.IsVisible = true;
+			_status = Status.Completed;
+			return _status;
+		}
+
+		public override void Terminate()
+		{
+		}
+
+		public override void AddSubGoal(Goal<Helper> goal)
+		{
+			throw new InvalidOperationException(
+				"Cannot add subgoals to an atomic goal");
+		}
+	}
+}
