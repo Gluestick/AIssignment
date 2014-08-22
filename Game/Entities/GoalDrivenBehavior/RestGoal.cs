5c857875b168075b6ae6799e25e726aacacc5c04
Joep Kemperman <mail@joepkemperman.nl>
2014-08-22 20:46:38 +0200

    RestGoal added.

---
+++ Game/Entities/GoalDrivenBehavior/RestGoal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ISGPAI.Game.Entities.GoalDrivenBehavior
+{
+	public class RestGoal : Goal<Helper>
+	{
+		private const int MaxRestIterations = 250;
+
+		private int _restIterations = 0;
+
+		public RestGoal(Helper owner)
+		{
+			this._owner = owner;
+		}
+
+		public override void Activate()
+		{
+			this._status = Status.Active;
+		}
+
+		public override Status Process()
+		{
+			_restIterations++;
+			if (_restIterations > MaxRestIterations)
+			{
+				_status = Status.Completed;
+				return _status;
+			}
+			return _status;
+		}
+
+		public override void Terminate()
+		{
+		}
+
+		public override void AddSubGoal(Goal<Helper> goal)
+		{
+			throw new InvalidOperationException(
+				"Cannot add subgoals to an atomic goal");
+		}
+	}
+}
